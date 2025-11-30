bdaa7d0d068813c02542bdb79dd7ef236e4ba8df
Jo23h <147503767+Jo23h@users.noreply.github.com>
2025-11-30 22:54:24 +0800

    basic task form done

--- Frontend/src/components/TaskBox/TaskForm.jsx
+++ Frontend/src/components/TaskBox/TaskForm.jsx
@@ -41,8 +41,99 @@ function TaskForm() {
         });
     };
 
+    const handleCancel = () => {
+      setNewTask({
+        taskName: '',
+        description: '',
+        date: '',
+        priority: '',
+        category: ''
+      });
+    };
+
   return (
-    <div>addTask</div>
+    <main>
+      <form onSubmit={handleSubmit}>
+
+        {/* Header section - Task name and description */}
+        <div className = 'header'>
+          <div>
+            <label htmlFor='task-name'>Task name</label>
+            <input 
+              type = 'text'
+              id = 'task'
+              name = 'taskName'
+              value = {newTask.taskName}
+              onChange = {handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor='description'>Description</label>
+            <input 
+              type = 'text'
+              id = 'description'
+              name = 'description'
+              value = {newTask.description}
+              onChange = {handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Middle row - Date and priority */}
+        <div className = 'middle-row'>
+
+          {/* (WIP) Date */}
+
+          {/* Priority */}
+          <select
+            id = 'priority'
+            name = 'priority'
+            value = {newTask.priority}
+            onChange = {handleChange}
+          >
+            <option value = 'priority-high'>Priority: High</option>
+            <option value = 'priority-medium'>Priority: Medium</option>
+            <option value = 'priority-low'>Priority: Low</option>
+          </select>
+        </div>
+
+        {/* Footer section - Category, cancel and add task */}
+        <div className = 'footer'>
+
+          {/* Category */}
+          <select
+            id = 'category'
+            name = 'category'
+            value = {newTask.category}
+            onChange = {handleChange}
+          >
+            <option value = 'inbox'>Inbox</option>
+            <option value = 'placeholder'>Placeholder</option>
+          </select>
+
+          {/* Cancel */}
+          <button
+            type = 'button'
+            onClick = {handleCancel}
+          >
+            Cancel
+          </button>
+
+          {/* Add task */}
+          <button type = 'submit'>
+            Add task
+          </button>
+        </div>
+        
+
+
+
+       
+
+      </form>
+    </main>
   )
 }
 
